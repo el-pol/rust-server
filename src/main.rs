d8844a41d091ed1d3fe191a4c6ab3c5288c250c1
Pol Milian <pol.milian.dev@gmail.com>
2023-02-09 15:57:49 +0800

    start a server and read stream

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    // Start server
+    let address = "127.0.0.1:8000";
+    let listener = TcpListener::bind(address).unwrap();
+
+    // Listen for connections
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+
+    fn handle_connection(mut stream: TcpStream) {
+        let mut buffer = [0; 1024];
+
+        stream.read(&mut buffer).unwrap();
+        println!("Stream received");
+        println!("{}", String::from_utf8_lossy(&buffer[..]))
+    }
 }
